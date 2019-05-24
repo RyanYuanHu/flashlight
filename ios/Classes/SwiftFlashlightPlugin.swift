cd8541e954a48865448fbd27b3ba4bd10c5fbd09
RyanYuanHu <redspider@163.com>
2019-05-24 16:43:28 +0800

    wip

--- ios/Classes/SwiftFlashlightPlugin.swift
+++ ios/Classes/SwiftFlashlightPlugin.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import AVFoundation
 
 public class SwiftFlashlightPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
@@ -9,6 +10,49 @@ public class SwiftFlashlightPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    switch (call.method) {
+    case "hasFlashlight":
+        result(self.hasFlashlight())
+        break
+    case "lightOn":
+        self.toggleFlash(on: true)
+        result(nil)
+        break
+    case "lightOff":
+        self.toggleFlash(on: false)
+        result(nil)
+        break
+    default:
+        result(FlutterMethodNotImplemented)
+        break
+    }
+    
   }
+    
+    private func hasFlashlight () -> Bool{
+        guard let device = AVCaptureDevice.default(for: AVMediaType.video) else { return false}
+        return device.hasTorch
+    }
+    
+    private func toggleFlash(on: Bool) {
+        guard let device = AVCaptureDevice.default(for: AVMediaType.video) else { return }
+        guard device.hasTorch else { return }
+        
+        do {
+            try device.lockForConfiguration()
+            if (on && device.torchMode == AVCaptureDevice.TorchMode.off) {
+                do {
+                    try device.setTorchModeOn(level: 1.0)
+                } catch {
+                    print(error)
+                }
+            } else if (!on && device.torchMode == AVCaptureDevice.TorchMode.on) {
+                device.torchMode = AVCaptureDevice.TorchMode.off
+            }
+            
+            device.unlockForConfiguration()
+        } catch {
+            print(error)
+        }
+    }
 }
